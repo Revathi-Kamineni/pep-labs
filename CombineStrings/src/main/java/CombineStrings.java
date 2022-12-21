fa1a60e5c0787357d36557a07a80e6bb1a80d315
Revathi-Kamineni <revathikamineni77@gmail.com>
2022-12-21 15:02:47 +0000

    Saved progress on CombineStrings 2022-12-21 15:02:47 +0000

--- CombineStrings/src/main/java/CombineStrings.java
+++ CombineStrings/src/main/java/CombineStrings.java
@@ -8,6 +8,10 @@ public class CombineStrings {
      * @return the combination of strings a and b.
      */
     public String combine(String a, String b){
-        return "";
+        return a+b;
+    }
+    public void main(String[] args){
+        CombineStrings s = new CombineStrings();
+        System.out.println(s.combine("abc" , "def"));
     }
 }
